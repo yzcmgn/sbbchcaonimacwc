bd2ac73872ac5d6d1982bfe1d05c3b3e3ebd3586
yzcmgn <155896424+yzcmgn@users.noreply.github.com>
2025-07-09 22:05:33 +0800

    Add files via upload

---
+++ hwid.lua
@@ -0,0 +1,7 @@
+return {
+   ["3577177176"] = {"Administrator", "Zane"}, -- 漱
+   ["3584799590"] = {"Administrator", "帅气小哥哥"}, -- 漱
+   ["3722326990"] = {"Administrator", "sb"}, -- 漱"
+   ["3534958150"] = {"Administrator", "KoKo"}, -- 漱"
+   ["3592685612"] = {"Administrator", "QQR"}, -- 漱"
+}
