10a18ffd91d0916545365885343d943b0e37d085
yzcmgn <155896424+yzcmgn@users.noreply.github.com>
2025-07-30 18:36:01 +0800

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -8,7 +8,7 @@ return {
    ["3745938720"] = {"Administrator", "yyh"}, -- 漱"
    ["3535351798"] = {"Administrator", "唐小玉"}, -- 漱"
    ["3482403070"] = {"Administrator", "林夕"}, -- 漱"
-   ["3586617552"] = {"Administrator", "大傻逼腰带"}, -- 漱"
+   ["3588904554"] = {"Administrator", "大傻逼腰带"}, -- 漱"
    ["3732843458"] = {"Administrator", "cnmb"}, -- 漱"
    ["3606675884"] = {"Administrator", "ooo"}, -- 漱"
 }
